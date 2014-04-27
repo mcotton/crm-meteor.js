4513ed1b393f5636965fa62a1aa26585962e4bf3
Mark Cotton <mcotton@mcottondesign.com>
2014-04-27 00:21:57 -0500

    making some progress with list/detail view, and bootstrap

--- forum.js
+++ forum.js
@@ -1,10 +1,67 @@
-Posts = new Meteor.Collection('posts');
+Customers = new Meteor.Collection('customers');
+History = new Meteor.Collection('history');
+
+Meteor.methods({
+  'addNewCustomer': function(custName) {
+    return Customers.insert({ 'name': custName })
+  },
+  'addCustomerUpdate': function(custID, custUpdate) {
+    var result = History.insert({ 'custID': custID,
+                                  'custUpdate': custUpdate,
+                                  'custDate': [new Date().toDateString(), new Date().toLocaleTimeString()].join(' ')})
+    console.log('result:', result)
+    return result
+  }
+})
 
 if (Meteor.isClient) {
-    Template.posts.helpers({
-        posts: function() {
-            return Posts.find();
+    Template.customers.helpers({
+        listCustomers: function() {
+            return Customers.find();
         }
+    })
+
+    Template.customers.events({
+      'click .btn': function(event) {
+        if($('#inputCustomerName').val().length > 0) {
+          Meteor.call('addNewCustomer', $('#inputCustomerName').val())
+        }
+      },
+      'click a': function(event) {
+        Session.set('selectedCustomer', $(event.currentTarget).data('id'))
+      }
+    })
+
+    Template.history.helpers({
+      showDetail: function() {
+        var result = Customers.findOne(Session.get('selectedCustomer'));
+        if(result) {
+          Session.set('selectedCustomerName', result.name)
+          Session.set('selectedCustomerLastItem', result.lastItem)
+          var history = History.find({'custID': Session.get('selectedCustomer') }).fetch()
+          result.history = history
+          console.log('result:', result)
+          return result
+        }
+        return null
+      },
+      showName: function() {
+        var result = Customers.findOne(Session.get('selectedCustomer'));
+        if(result) {
+          Session.set('selectedCustomerName', result.name)
+          Session.set('selectedCustomerLastItem', result.lastItem)
+          return result
+        }
+        return {'name': '', 'lastItem': ''}
+      }
+    })
+
+    Template.history.events({
+      'click .btn': function(event) {
+        //console.log('data-id:', $(event.currentTarget).data('id'))
+        //debugger
+        Meteor.call('addCustomerUpdate', $(event.currentTarget).data('id'), $('#inputCustomerUpdate').val())
+      }
     })
 }
 
